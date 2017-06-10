0157d6667b309ed2cc35a8f7682698e1a3e7603f
Kitty <thepinkcodemonkey@gmail.com>
2017-06-10 03:34:12 +0000

    Added initial script for the CLI

---
+++ app.js
@@ -0,0 +1,35 @@
+const readline = require('readline');
+
+let rl = readline.Interface({
+    input: process.stdin,
+    output: process.stdout 
+});
+
+function cleanUp() {
+    console.log("Let's continue next time");
+    rl.close();
+}
+
+console.log(process.cwd());
+
+rl.on("line", input => {
+   let trimmedInput = input.trim().toLocaleLowerCase();
+   
+   switch (trimmedInput) {
+       case 'exit':
+           rl.close();
+            break;
+        case 'prev':
+            console.log("Going back to previous lesson.");
+            break;
+        case 'next':
+            console.log("Let's move to the next lesson.");
+            break;
+        default:
+            console.log("I don't understand what you just said.");
+            break;
+   }
+})
+.on("close", () => {
+    cleanUp();
+});
